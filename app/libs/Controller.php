fa4e1baac795a6a75c8667f8effbc1b5930c24a0
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-29 01:13:04 -0800

    Create controller.php

---
+++ app/libs/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+class Controller 
+{
+    protected $view;
+
+    public function go($controller, $action, $param = '')
+    {
+        $goingTo = "Location: " . URLROOT . "/" . $controller . "/" . $action . "/" . $param ;
+        header($goingTo);
+    }
+
+    public function goBack()
+    {
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+
+    public function setView($viewFile, $viewData = []) 
+    {
+        $this->view = new View($viewFile, $viewData);
+        return $this->view;
+    }
+
+    public function model($model)
+    {
+        $model = ucfirst($model);
+        require_once MODEL . $model . '.php';
+        return new $model;
+    }
+}
