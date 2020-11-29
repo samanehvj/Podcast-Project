401df4ab4699f5bb1ceb0faed5e84a9056bfee0d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-29 01:56:23 -0800

    Edit CSS index.php

--- app/views/index.php
+++ app/views/index.php
@@ -6,14 +6,13 @@
 <div class="row my-5 ">
 
     <div class="col-md-6 my-2 text-center title-episode ">
-        <h2>Latest Episode </h>
+        <h2 class="last">Latest Episode </h2>
 
-            <img class="img-fluid w-100 mt-5 mb-2" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1354630782l/16098322.jpg" alt="">
+            <img class="img-fluid w-75 mt-5 mb-2 ml-2 " src="/imgs/image1.png">
             <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/16964270/height/90/theme/custom/thumbnail/no/direction/backward/render-playlist/no/custom-color/414300/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>    </div>  
     
     <div class="col-md-6 my-2 d-flex flex-column justify-content-around text-center title-episode ">
-        <!-- we will replace it with php coe to fetch data from databse -->
-        <h2>Earlier Episodes</h2>
+        <h2 class="mb-5">Earlier Episodes</h2>
 
         <div class="row mb-2">
 
