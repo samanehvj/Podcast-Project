d747fb6a86ffb86465720199f5da65bc13c02ee8
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-29 01:12:49 -0800

    Edit Css

--- app/views/episode.php
+++ app/views/episode.php
@@ -11,10 +11,12 @@
             </a>
             <div class="row mt-2">
                 <div class="col-12">
-                    <h5 class="p-2 ml-2"> <?= $episode->title ?>  </h5>
-                    <a class="btn btn-warning  text-center ml-5 mb-5 " href="<?= $episode->link ?>" target="_blank" > 
+                    <h5 class="p-2 ml-2"> <?= $episode->title ?></h5>
+                    <div class="text-center">
+                    <a class="btn btn-warning text-center ml-2 mb-5 " href="<?= $episode->link ?>" target="_blank" > 
                         Listen Now
                     </a>
+                    </div>
                 </div>
             </div>
         </div>
