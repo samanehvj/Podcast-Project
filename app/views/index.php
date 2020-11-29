a3f27b834b4cbd521452b2136879bc907e733839
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-28 23:50:23 -0800

    Create index.php

---
+++ app/views/index.php
@@ -0,0 +1,48 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+<!-- <div class="container-fluid"> -->
+<img src="/imgs/hero1.jpg" class="img-fluid w-100 ">
+<!-- </div> -->
+<div class="row my-5 ">
+
+    <div class="col-md-6 my-2 text-center title-episode ">
+        <h2>Latest Episode </h>
+
+            <img class="img-fluid w-100 mt-5 mb-2" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1354630782l/16098322.jpg" alt="">
+            <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/16964270/height/90/theme/custom/thumbnail/no/direction/backward/render-playlist/no/custom-color/414300/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>    </div>  
+    
+    <div class="col-md-6 my-2 d-flex flex-column justify-content-around text-center title-episode ">
+        <!-- we will replace it with php coe to fetch data from databse -->
+        <h2>Earlier Episodes</h2>
+
+        <div class="row mb-2">
+
+            <div class="col-md-4 mb-1">
+                <img class="img-fluid w-100" src="https://s13emagst.akamaized.net/products/22280/22279205/images/res_bd8a509f6c9769d937da3784794817b8.jpg?width=300&height=300&hash=A29C063E750768B66C004B85714BB0DF" alt="">
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+            <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/16781975/height/90/theme/custom/thumbnail/no/direction/backward/render-playlist/no/custom-color/414300/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>            </div>
+        </div>
+        <!-- -->
+
+        <div class="row mb-2">
+
+            <div class="col-md-4 mb-1">
+                <img class="img-fluid w-100" src="https://i.pinimg.com/originals/e0/e3/5a/e0e35ae45d372c2de93401366e9ef1cf.jpg" alt="">
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+            <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/16579055/height/90/theme/custom/thumbnail/no/direction/backward/render-playlist/no/custom-color/414300/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>            </div>
+        </div>
+
+        <div class="row mb-2">
+            <div class="col-md-4 mb-1">
+                <img class="img-fluid w-100" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320405766i/466756._UY450_SS450_.jpg" alt="">
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+            <iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/16410140/height/90/theme/custom/thumbnail/no/direction/backward/render-playlist/no/custom-color/414300/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>        </div>
+
+    </div>
+</div>
+
+
+<?php include VIEW . 'layouts/footer.php' ?>
