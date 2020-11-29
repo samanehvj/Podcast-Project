e4c45ae8cef2994a3fcf42a043d88d7b91bc5b73
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-28 23:53:34 -0800

    Create about.php

---
+++ app/views/about.php
@@ -0,0 +1,51 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+
+
+
+
+<div class="row w-100 about-hero p-3 pb-5">
+  <div class="col-lg-8">
+
+    <p class="text-dark about-hero-text p-lg-5">
+      Candace and Jess meet in the podcast basement bi-weekly to take turns judging a book by its cover. They do not read the books, but rather tell you the story they see hidden within the cover art. Each episode features one book, lots of laughs, friendly banter, and probably a few song suggestions along the way. If you're looking for a light hearted comedy podcast, that makes you feel like you're hanging out with your best buds then look no further. New episodes every other Thursday!
+    </p>
+  </div>
+  <div class="col-lg-4">
+    <p class="text-dark about-hero-text p-lg-5">
+      You can listen to Candace and Jess on
+    </p>
+    <div class="px-lg-5">
+      <a href="https://open.spotify.com/show/6uvK4fFQjQDVPUf4P9xYN4"><img src="/imgs/spotify.png" alt="spotify" /></a>
+      <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9jYW5kYWNlYW5kamVzcy5saWJzeW4uY29tL3Jzcw?sa=X&ved=2ahUKEwjDysX_9cvsAhUBx54KHZFbC40Q9sEGegQIARAH"><img src="/imgs/google.png" alt="google" /></a>
+      <a href="https://podcasts.apple.com/ca/podcast/candace-and-jess-judge-a-book-by-its-cover/id1435982288"><img src="/imgs/apple.png" alt="Apple" /></a>
+      <a href="https://www.stitcher.com/show/candace-and-jess-judge-a-book-by-its-cover"><img src="/imgs/Stitche.png
+" alt="Apple" /></a>
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+
+
+  <div class="card-deck">
+    <div class="card">
+      <img class="card-img-top " src="/imgs/CANDACE.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Candace's Bio:</h5>
+        <p class="card-text">Candace is a podcaster/music lover who enjoys long walks, her pals, playing guitar and painting. Graduating from VCC in 2015 Candace continues to expand her love for art and all things radio. She’s the kind of girl that will tell you “THIS IS MY FAVOURITE SONG” as many times as she could in one day.
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="/imgs/jess.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Jess' Bio:</h5>
+        <p class="card-text">Jess is a podcaster, writer and broadcasting student. He lives in Vancouver BC. He is a former professional cook, he has hung up his kitchen spurs vowing that the only kitchen he will ever cook in again will be his own. He also presents on UK radio station Winchester Today.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include VIEW . 'layouts/footer.php' ?>
