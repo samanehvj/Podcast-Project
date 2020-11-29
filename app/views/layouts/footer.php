9eba7c93c0c8d116a2458e4cd502679d725ce6c8
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-28 23:50:32 -0800

    Create footer.php

---
+++ app/views/layouts/footer.php
@@ -0,0 +1,24 @@
+</div><!-- end of container -->
+<footer class="section">
+        <p>©2020 All rights reserved | Candace and Jess
+        </p>
+        <ul>
+                <!-- <li><a target="_blank" class="spotify" href="https://open.spotify.com/show/6uvK4fFQjQDVPUf4P9xYN4"><i class="fab fa-spotify"></i></a></li>
+                <li><a target="_blank" class="podcast" href="https://podcasts.apple.com/ca/podcast/candace-and-jess-judge-a-book-by-its-cover/id1435982288"><i class="fas fa-podcast"></i></a></li>
+                <li><a target="_blank" class="youtube" href="https://www.youtube.com/"><i class="fa fa-fw fa-inverse fa-youtube"></i></a></li>
+                <li><a target="_blank" class="vimeo" href="https://www.vimeo.com/"><i class="fa fa-fw fa-inverse fa-vimeo-square"></i></a></li> -->
+
+
+                <div class="col secondary">
+                        <a href="https://open.spotify.com/show/6uvK4fFQjQDVPUf4P9xYN4"><img src="/imgs/spotify.png" alt="spotify" /></a>
+                        <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9jYW5kYWNlYW5kamVzcy5saWJzeW4uY29tL3Jzcw?sa=X&ved=2ahUKEwjDysX_9cvsAhUBx54KHZFbC40Q9sEGegQIARAH"><img src="/imgs/google.png" alt="google" /></a>
+                        <a href="https://podcasts.apple.com/ca/podcast/candace-and-jess-judge-a-book-by-its-cover/id1435982288"><img src="/imgs/apple.png" alt="Apple" /></a>
+                        <a href="https://www.stitcher.com/show/candace-and-jess-judge-a-book-by-its-cover"><img src="/imgs/Stitche.png
+" alt="Apple" /></a>
+
+                </div> <!-- colsecondary -->
+        </ul>
+</footer>
+</body>
+
+</html>
