0db142894b36a3a956721ad6893aabb54044c6f2
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-29 01:12:55 -0800

    Create Episode.php

---
+++ app/models/Episode.php
@@ -0,0 +1,71 @@
+<?php
+
+class Episode {
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new DB;
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM episodes";
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getById($id)
+    {
+        $sql = "SELECT * FROM episodes WHERE id = :id";
+        $this->db->query($sql);
+        $this->db->bind(':id', $id);
+
+        return $this->db->single();
+    }
+    
+
+    public function add($title, $img, $iframe, $link)
+    {
+        $sql = "INSERT INTO episodes 
+        (title, img, iframe, link) 
+        values 
+        (:title, :img, :iframe, :link)";
+
+        $this->db->query($sql);
+
+        $this->db->bind(':title', $title);
+        $this->db->bind(':img', $img);
+        $this->db->bind(':iframe', $iframe);
+        $this->db->bind(':link', $link);
+
+    
+        return $this->db->execute();
+    }
+
+    public function edit($id, $title, $img, $iframe, $link)
+    {
+        $sql = "UPDATE episodes 
+        SET  title=:title, img=:img, iframe=:iframe, link=:link
+        WHERE id=:id";
+
+        $this->db->query($sql);
+
+        $this->db->bind(':title', $title);
+        $this->db->bind(':img', $img);
+        $this->db->bind(':iframe', $iframe);
+        $this->db->bind(':link', $link);
+        $this->db->bind(':id', $id);
+
+        return $this->db->execute();
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM episodes WHERE id = :id";
+        $this->db->query($sql);
+        $this->db->bind(':id', $id);
+
+        return $this->db->execute();
+    }
+}
