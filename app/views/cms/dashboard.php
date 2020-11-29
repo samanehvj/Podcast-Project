fbef4f5d5760bf058cc1fb5b62b2d7cd70883a49
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-28 23:53:26 -0800

    Create dashboardt.php

---
+++ app/views/cms/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+include VIEW . 'cms/layouts/header.php';
+include VIEW . 'cms/layouts/sidebar.php';
+
+?>
+<div class="table-responsive">
+
+<h1>Hi Admin</h1>
+</div>
+<?php
+
+include VIEW . 'cms/layouts/footer.php';
