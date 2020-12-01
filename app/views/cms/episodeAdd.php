04e9d02a12873b163ff2c8489b8b1d76e33a7596
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-30 20:59:00 -0800

    Edit episodeAdd.php

--- app/views/cms/episodeAdd.php
+++ app/views/cms/episodeAdd.php
@@ -5,7 +5,7 @@ include VIEW . 'cms/layouts/sidebar.php';
 
 ?>
 <div class="container d-flex justify-content-around ">
-    <form class="mt-5" method="post" action="/episode/adminDoAdd" >
+    <form class="mt-5 col-md-8" method="post" action="/episode/adminDoAdd" >
         
 
         <div class="form-group">
@@ -16,12 +16,13 @@ include VIEW . 'cms/layouts/sidebar.php';
             <input  class="form-control" id="img" name="img" placeholder="Episode Imgae Link" required />
         </div>
 
-        <div class="form-group">
-            <textarea row="4" class="form-control" id="iframe" name="iframe" placeholder="Episode Iframe" required ></textarea>
-        </div>
 
         <div class="form-group">
             <input  class="form-control" id="link" name="link" placeholder="Episode Link" required />
+        </div>
+
+        <div class="form-group">
+            <textarea row="4" class="form-control" id="iframe" name="iframe" placeholder="Episode Iframe Src"  ></textarea>
         </div>
 
 
